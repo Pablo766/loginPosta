09b7351432bd1fde924d19b7718e728d0cdd6d7a
Pablo766 <pablomaxit@hotmail.com>
2018-03-31 15:49:41 -0300

    Conexion con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA40OYYPd6oo9KkzUVg_vgtKNh0QU7L818",
+    authDomain: "loginapp-8b570.firebaseapp.com",
+    databaseURL: "https://loginapp-8b570.firebaseio.com",
+    projectId: "loginapp-8b570",
+    storageBucket: "loginapp-8b570.appspot.com",
+    messagingSenderId: "250541030835"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA40OYYPd6oo9KkzUVg_vgtKNh0QU7L818",
+    authDomain: "loginapp-8b570.firebaseapp.com",
+    databaseURL: "https://loginapp-8b570.firebaseio.com",
+    projectId: "loginapp-8b570",
+    storageBucket: "loginapp-8b570.appspot.com",
+    messagingSenderId: "250541030835"
+  }
 };
